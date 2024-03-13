4d859fa805172c2921867917154e7b37423f27f7
myconpeter <79177019+myconpeter@users.noreply.github.com>
2024-03-13 23:39:03 +0100

    'df'

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import mongoose from 'mongoose'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 // import connectDB from './db/db.js'
 
 
@@ -13,12 +14,48 @@ import { notFound, errorHandler } from './middleware/errorMiddle.js'
 const port = process.env.Port || 8000
 const app = express()
 
+// deployment
+
+if (process.env.NODE_ENV === "production") {
+    const __dirname = path.resolve()
+    app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+    );
+    console.log('joining')
+
+} else {
+    app.get('/', (req, res) => res.send('Server is ready!'))
+}
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use('/api/users', userRoutes)
 app.use(notFound)
 app.use(errorHandler)
+
+
+// deployment
+
+// if (process.env.NODE_ENV === "production") {
+//     const __dirname = path.resolve()
+//     app.use(express.static(path.join(__dirname, 'frontend/dist')))
+
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+//     );
+//     console.log('joining')
+
+// } else {
+//     app.get('/', (req, res) => res.send('Server is ready!'))
+// }
+
+
+
+
+
 
 
 
