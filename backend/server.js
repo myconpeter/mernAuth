f42a4669a4b24867e4d84990235fbe190307a176
myconpeter <79177019+myconpeter@users.noreply.github.com>
2024-04-04 23:23:10 +0100

    lets see

--- backend/server.js
+++ backend/server.js
@@ -37,20 +37,7 @@ app.use(notFound)
 app.use(errorHandler)
 
 
-// deployment
 
-// if (process.env.NODE_ENV === "production") {
-//     const __dirname = path.resolve()
-//     app.use(express.static(path.join(__dirname, 'frontend/dist')))
-
-//     app.get('*', (req, res) =>
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-//     );
-//     console.log('joining')
-
-// } else {
-//     app.get('/', (req, res) => res.send('Server is ready!'))
-// }
 
 
 
